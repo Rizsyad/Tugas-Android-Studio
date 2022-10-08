04e92c252487b7e6ebad333ddaa00740619916ac
rizsyad <rizsyad@gmail.com>
2022-10-08 15:13:31 +0700

    Tugas 01

---
+++ 01_1912500467_Rizsyad_AF/app/src/main/java/com/bl/a01_1912500467_rizsyad_af/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bl.a01_1912500467_rizsyad_af;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
